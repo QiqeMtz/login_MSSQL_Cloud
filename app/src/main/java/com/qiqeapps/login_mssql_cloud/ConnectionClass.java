0ef33e798df36ed85cda804f9b0c329135e978a0
Enrique <lejm.18@gmail.com>
2016-03-09 04:59:40 -0600

    Initial commit

---
+++ app/src/main/java/com/qiqeapps/login_mssql_cloud/ConnectionClass.java
@@ -0,0 +1,43 @@
+package com.qiqeapps.login_mssql_cloud;
+
+/**
+ * Created by Enrique on 07/03/16.
+ */
+
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.util.Log;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionClass {
+    String ip = "enrique18.database.windows.net";
+    String classs = "net.sourceforge.jtds.jdbc.Driver";
+    String db = "BD_ANDROID_CONN";
+    String un = "QiqeMtz";
+    String password = "Seusers512";
+
+    @SuppressLint("NewApi")
+    public Connection CONN() {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection conn = null;
+        String ConnURL = null;
+        try {
+            Class.forName(classs);
+            ConnURL = "jdbc:jtds:sqlserver://" + ip + ";"
+                    + "databaseName=" + db + ";user=" + un + ";password="
+                    + password + ";";
+            conn = DriverManager.getConnection(ConnURL);
+        } catch (SQLException se) {
+            Log.e("ERRO", se.getMessage());
+        } catch (ClassNotFoundException e) {
+            Log.e("ERRO", e.getMessage());
+        } catch (Exception e) {
+            Log.e("ERRO", e.getMessage());
+        }
+        return conn;
+    }
+}
